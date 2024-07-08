df27ec0fe69519bcfbedf48edfc8c86a8caf1d48
Olee <olee@Oles-Air.fritz.box>
2024-07-08 21:51:17 +0200

    Style

--- app/TimeRangeToggle.tsx
+++ app/TimeRangeToggle.tsx
@@ -8,17 +8,30 @@ export const TimeRangeToggle = ({
   setTimeRange: (timeRange: TimeRange) => void;
 }) => {
   const timeRanges: TimeRange[] = ["short_term", "medium_term", "long_term"];
+  const label: Record<TimeRange, string> = {
+    short_term: "Short",
+    medium_term: "Medium",
+    long_term: "Long",
+  };
   return (
-    <div className="flex justify-center space-x-1 border w-fit rounded-lg px-2 py-1 text-sm">
-      {timeRanges.map((timeRange) => (
-        <button
-          key={timeRange}
-          onClick={() => setTimeRange(timeRange)}
-          className={`${selectedTimeRange === timeRange ? "underline" : ""}`}
-        >
-          {timeRange.split("_")[0]}
-        </button>
-      ))}
+    <div className="flex justify-center w-fit text-sm">
+      {timeRanges.map((timeRange) => {
+        const isFirst = timeRange === timeRanges[0];
+        const isLast = timeRange === timeRanges[timeRanges.length - 1];
+        return (
+          <button
+            key={timeRange}
+            onClick={() => setTimeRange(timeRange)}
+            className={`${
+              selectedTimeRange === timeRange ? "bg-slate-400 text-white" : ""
+            } ${isLast ? "rounded-r-lg" : "border-r"} ${
+              isFirst ? "rounded-l-lg" : ""
+            } px-2 py-1 border`}
+          >
+            {label[timeRange]}
+          </button>
+        );
+      })}
     </div>
   );
 };
